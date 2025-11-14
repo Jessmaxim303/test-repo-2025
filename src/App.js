b6ef2373d70a2f512991f243dd5e6618de10d6ef
JessMaxim303 <jessmaxim303@JessMaxim303s-Mac-mini.local>
2025-11-14 12:38:47 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-Header">
+	    <div className="Menu-main-container">
+	  
+	  	  <container className="Menu-container">Home</container>
+	      <container className="Menu-container">Work</container>
+	      <container className="Menu-container"> About</container>
+	    </div>
+	   </div>
+	  <div className="App-Welcome">
+	    <div className="App-Welcome-Subcontainer">
+	  <img src="" /> 
+	    </div>
+	    <div className="App-Welcome-Subcontainer">Welcome Right</div>
+	  </div>
+	  <div className="App-Body">Body</div>
+	  <div className="App-Solution">Solution</div>
+	  <div className="App-DataProof">Data Proof</div>
+	  <div className="App-Data">Data</div>
+	  
     </div>
   );
 }
